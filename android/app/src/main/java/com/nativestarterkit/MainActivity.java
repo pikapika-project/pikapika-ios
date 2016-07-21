e6fb22c837799edc9a5c99273b24632e743e927a
Sebastián Osorio <im@svzosorio.co>
2016-07-21 12:28:36 -0500

    go to the start point :(

--- android/app/src/main/java/com/nativestarterkit/MainActivity.java
+++ android/app/src/main/java/com/nativestarterkit/MainActivity.java
@@ -1,6 +1,8 @@
 package com.nativestarterkit;
 
 import com.facebook.react.ReactActivity;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
@@ -34,7 +36,9 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
-            new MainReactPackage()
+            new MainReactPackage(),
+            new VectorIconsPackage(),
+            new CodePush(null, this, BuildConfig.DEBUG)
         );
     }
 }
