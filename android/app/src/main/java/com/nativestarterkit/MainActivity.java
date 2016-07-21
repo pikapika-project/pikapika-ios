0dd9d05ac47c36ab311e26719263a7f93aa14388
Sebastián Osorio <im@svzosorio.co>
2016-07-20 20:37:40 -0500

    react-native upgrade

--- android/app/src/main/java/com/nativestarterkit/MainActivity.java
+++ android/app/src/main/java/com/nativestarterkit/MainActivity.java
@@ -1,8 +1,6 @@
 package com.nativestarterkit;
 
 import com.facebook.react.ReactActivity;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
@@ -36,9 +34,7 @@ public class MainActivity extends ReactActivity {
     @Override
     protected List<ReactPackage> getPackages() {
         return Arrays.<ReactPackage>asList(
-            new MainReactPackage(),
-            new VectorIconsPackage(),
-            new CodePush(null, this, BuildConfig.DEBUG)
+            new MainReactPackage()
         );
     }
 }
